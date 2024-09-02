b1ae11b4a2e1f1a74e4f283947b92a084970a8f3
Shalin Patel <42554035+PatelShalin@users.noreply.github.com>
2024-09-01 23:26:44 -0400

    yp-scraper-1.0

---
+++ README.md
@@ -0,0 +1,10 @@
+## YP Scraper
+### About
+YP Scraper is a selenium powered webscraper designed to scrape Names and Phone Numbers from businesses within a given city. It will output these names and phone numbers into a csv file.
+
+### Usage
+1. Ensure you have Poetry installed: https://python-poetry.org/docs/#installation
+
+2. Install the dependencies `poetry install`
+
+3. Run the scraper `poetry run python yp_scraper/scraper.py {city}`
---
+++ yp_scraper/scraper.py
@@ -0,0 +1,98 @@
+import argparse
+import csv
+import os
+
+from datetime import datetime
+
+from selenium import webdriver
+from selenium.webdriver.common import by
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+def scrape(city: str):
+    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    out_dir = f'{city}@{timestamp}'
+    os.makedirs(out_dir, exist_ok=True)
+
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
+
+    driver.get(f'https://www.yellowpages.ca/locations/Ontario/{city}/')
+
+    categories = driver.find_elements(by.By.CLASS_NAME, "categories-list")
+
+    url_stack = []
+    for category in categories[1:]:
+        list_elems = category.find_elements(by.By.CLASS_NAME, "resp-list")
+        if list_elems[-1].text == "View All":
+            view_all = list_elems[-1]
+            view_all_a = view_all.find_element(by.By.XPATH, "a")
+            url = view_all_a.get_attribute("href")
+            url_stack.append(url)
+        else:
+            for list_elem in list_elems:
+                list_elem_a = list_elem.find_element(by.By.XPATH, "a")
+                url = list_elem_a.get_attribute("href")
+                url_stack.append(url)
+
+    while url_stack:
+        url = url_stack.pop(0)
+        try:
+            driver.get(url)
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((by.By.CLASS_NAME, "categories-list")))
+        except TimeoutException:
+            print(f"Timed out while trying to scrape {url}")
+            
+        categories = driver.find_elements(by.By.CLASS_NAME, "categories-list")
+
+        if not categories:
+            listings = driver.find_elements(by.By.CLASS_NAME, "listing")
+            if not listings:
+                continue
+            header = driver.find_element(by.By.CLASS_NAME, "page__container-title").text
+            csv_path = os.path.join(out_dir, f'{header}.csv')
+            with open(csv_path, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(["Name", "Phone Number"])
+                listing_phone_numbers = driver.find_elements(by.By.XPATH, '//*[@title="Get the Phone Number"]')
+                page = True
+                while page:
+                    for i, listing in enumerate(listings):
+                        listing_name = listing.find_element(by.By.CLASS_NAME, "listing__name").text
+                        listing_phone_number = listing_phone_numbers[i].get_attribute('data-phone')
+                        writer.writerow([listing_name, listing_phone_number])
+                    next_page_div = driver.find_element(by.By.CLASS_NAME, "view_more_section_noScroll")
+                    next_page_a = next_page_div.find_elements(by.By.XPATH, "a")
+                    if next_page_a and next_page_a[-1].text == "Next >>":
+                        next_page_url = next_page_a[-1].get_attribute("href")
+                        driver.get(next_page_url)
+                        listings = driver.find_elements(by.By.CLASS_NAME, "listing")
+                        listing_phone_numbers = driver.find_elements(by.By.XPATH, '//*[@title="Get the Phone Number"]')
+                        page = True
+                    else:
+                        page = False
+
+        for category in categories:
+            list_elems = category.find_elements(by.By.CLASS_NAME, "resp-list")
+            if list_elems[-1].text == "View All":
+                view_all = list_elems[-1]
+                view_all_a = view_all.find_element(by.By.XPATH, "a")
+                url = view_all_a.get_attribute("href")
+                url_stack.append(url)
+            else:
+                for list_elem in list_elems:
+                    list_elem_a = list_elem.find_element(by.By.XPATH, "a")
+                    url = list_elem_a.get_attribute("href")
+                    url_stack.append(url)
+
+    driver.quit()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('city', type=str, help='The city in which you would like to scrape business info.')
+
+    args = parser.parse_args()
+    scrape(args.city)
